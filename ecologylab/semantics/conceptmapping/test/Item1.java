804dbee01fbbca598dcaad3f084d9e0fa41b40b3
quyin <quyin@ecologylab.net>
2010-04-07 17:16:38 +0000

    the @xml_scope example updated. nearly fixed.

--- ecologylab/semantics/conceptmapping/test/Item1.java
+++ ecologylab/semantics/conceptmapping/test/Item1.java
@@ -1,8 +1,10 @@
 package ecologylab.semantics.conceptmapping.test;
 
 import ecologylab.xml.xml_inherit;
+import ecologylab.xml.ElementState.xml_tag;
 
 @xml_inherit
+@xml_tag("item1")
 public class Item1 extends Item
 {
 	public Item1()
--- ecologylab/semantics/conceptmapping/test/ItemList.java
+++ ecologylab/semantics/conceptmapping/test/ItemList.java
@@ -13,28 +13,41 @@ public class ItemList extends ElementState
 {
 	@xml_nowrap
 	@xml_collection
-	@xml_scope(ItemTScope.ITEM_TSCOPE)
-	// @xml_classes({ItemList.class, Item.class, Item1.class, Item2.class})
-	private ArrayList	list;
+  @xml_scope(ItemTScope.ITEM_TSCOPE)
+//	@xml_classes({ItemList.class, Item.class, Item1.class, Item2.class})
+	private ArrayList<Item>	list;
 
 	public ArrayList<Item> getList()
 	{
 		return list;
 	}
+	
+	public void addItem(Item it)
+	{
+		list.add(it);
+	}
 
 	public ItemList()
 	{
-
+		list = new ArrayList<Item>();
 	}
 
 	public static void main(String[] args) throws XMLTranslationException
 	{
+		ItemTScope.get();
+		
+		ItemList il0 = new ItemList();
+		il0.addItem(new Item1());
+		il0.addItem(new Item2());
+		il0.addItem(new Item1());
+		System.out.println(il0.translateToXML().toString());
+		
 		ItemList il = (ItemList) ItemList.translateFromXML(
 				"ecologylab/semantics/conceptmapping/test/test.xml", ItemTScope.get());
 
 		for (Item i : il.getList())
 		{
-			System.out.println(i.getClassName());
+			System.out.println(((Item1)i).getClassName());
 		}
 	}
 }
