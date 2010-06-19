91ca46bb4538f2d175aec3923fd2238177f6a84f
quyin <quyin@ecologylab.net>
2010-06-19 21:52:25 +0000

    disambiguation tuning ready.

--- ecologylab/semantics/conceptmapping/model/tuning/Tuning.java
+++ ecologylab/semantics/conceptmapping/model/tuning/Tuning.java
@@ -1,9 +1,7 @@
 package ecologylab.semantics.conceptmapping.model.tuning;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONException;
@@ -17,7 +15,6 @@ import qmlt.dataset.filter.MultiWayDiscretizingFilter;
 import qmlt.dataset.filter.OutputConversionFilter;
 import qmlt.decisiontree.DecisionTree;
 import qmlt.decisiontree.control.DecisionTreeTrainingController;
-import qmlt.decisiontree.prune.PessimisticPruner;
 import qmlt.decisiontree.prune.ReducedErrorPruner;
 import qmlt.decisiontree.treebuild.GainRatioTreeBuildController;
 import qmlt.decisiontree.treebuild.InfoGainTreeBuildController;
@@ -35,11 +32,13 @@ public class Tuning
 	private DataSet	dValidateSet;
 	private DataSet	dTestSet;
 	
-	private List<Configuration> configs;
+	private DecisionTreeTrainingController[]	dtCtrls;
+
+	private SVMTrainingController[]	svmCtrls;
 
 	protected void generateDatasets() throws IOException, JSONException
 	{
-		DataSet ds = BasicDataSet.read("data/disambi.qmlt.ds", "data/disambi2000.qmlt.dat");
+		DataSet ds = BasicDataSet.read("data/disambi.qmlt.ds", "data/disambi.qmlt.dat");
 		System.out.println("" + ds.getInstances().size() + " instances read.");
 		ds = ds.filter(new GaussianNormalizationFilter()).filter(new OutputConversionFilter("true", 1, "false", -1, 0));
 		System.out.println("data preprocessed.");
@@ -58,17 +57,17 @@ public class Tuning
 	
 	protected void generateConfigurations()
 	{
-		DecisionTreeTrainingController[] dtCtrls = new DecisionTreeTrainingController[] {
+		dtCtrls = new DecisionTreeTrainingController[] {
 				new DecisionTreeTrainingController(new InfoGainTreeBuildController(), new ReducedErrorPruner(dValidateSet)),
-				new DecisionTreeTrainingController(new InfoGainTreeBuildController(), new PessimisticPruner(dValidateSet)),
+				// new DecisionTreeTrainingController(new InfoGainTreeBuildController(), new PessimisticPruner(dValidateSet)),
 				new DecisionTreeTrainingController(new GainRatioTreeBuildController(), new ReducedErrorPruner(dValidateSet)),
-				new DecisionTreeTrainingController(new GainRatioTreeBuildController(), new PessimisticPruner(dValidateSet))
+				// new DecisionTreeTrainingController(new GainRatioTreeBuildController(), new PessimisticPruner(dValidateSet))
 		};
 		
 		double[] Cs = {0.25, 0.5, 1, 2, 4};
-		double[] gammas = {0.25, 0.5, 1, 2, 4};
+		double[] gammas = {0.125, 0.25, 0.5, 1, 2, 4};
 		
-		SVMTrainingController[] svmCtrls = new SVMTrainingController[Cs.length * gammas.length];
+		svmCtrls = new SVMTrainingController[Cs.length * gammas.length];
 		for (int i = 0; i < Cs.length; ++i)
 		{
 			for (int j = 0; j < gammas.length; ++j)
@@ -76,51 +75,42 @@ public class Tuning
 				svmCtrls[i*gammas.length + j] = new SVMTrainingController(Cs[i], gammas[j]);
 			}
 		}
-		
-		configs = new ArrayList<Configuration>();
-		for (int i = 0; i < dtCtrls.length; ++i)
-		{
-			for (int j = 0; j < svmCtrls.length; ++j)
-			{
-				configs.add(new Configuration(dtCtrls[i], svmCtrls[j]));
-			}
-		}
 	}
 	
-	protected float tuneOnDecisionTree(Configuration config)
+	protected float tuneOnDecisionTree(DecisionTreeTrainingController ctrl)
 	{
 		DecisionTree dt = new DecisionTree();
-		dt.train(dTrainSet, config.dtCtrl);
+		dt.train(dTrainSet, ctrl);
 		ClassificationAccuracyEvaluator<DecisionTree> eva = new ClassificationAccuracyEvaluator<DecisionTree>();
 		float accuracy = eva.evaluate(dt, dTestSet);
 		return accuracy;
 	}
 	
-	protected float tuneOnSVM(Configuration config)
+	protected float tuneOnSVM(SVMTrainingController ctrl)
 	{
 		SVM svm = new SVM();
-		svm.train(trainSet, config.svmCtrl);
+		svm.train(trainSet, ctrl);
 		ClassificationAccuracyEvaluator<SVM> eva = new ClassificationAccuracyEvaluator<SVM>();
 		float accuracy = eva.evaluate(svm, testSet);
 		return accuracy;
 	}
 
-	public void tune() throws IOException, JSONException
+	public void tune()
 	{
-		generateDatasets();
-		generateConfigurations();
-		
-		out.println("DecisionTree\tSVM\n");
-		for (Configuration config : configs)
+		for (DecisionTreeTrainingController dtCtrl : dtCtrls)
 		{
-			float a1 = tuneOnDecisionTree(config);
-			float a2 = tuneOnSVM(config);
-			out.println("" + a1 + "\t" + a2);
+			float a = tuneOnDecisionTree(dtCtrl);
+			out.println("tree[" + dtCtrl + "]:" + a);
+		}
+		for (SVMTrainingController svmCtrl : svmCtrls)
+		{
+			float a = tuneOnSVM(svmCtrl);
+			out.println("svm[" + svmCtrl + "]:" + a);
 		}
 		out.flush();
 	}
 	
-	public Tuning(String outputFilepath) throws FileNotFoundException
+	public Tuning(String outputFilepath) throws IOException, JSONException
 	{
 		if (outputFilepath == null)
 		{
@@ -130,6 +120,9 @@ public class Tuning
 		{
 			out = new PrintStream(outputFilepath);
 		}
+		
+		generateDatasets();
+		generateConfigurations();
 	}
 	
 	public static void main(String[] args) throws IOException, JSONException
@@ -138,7 +131,7 @@ public class Tuning
 		
 		for (int i = 0; i < numPairs; ++i)
 		{
-			Tuning t = new Tuning(null);
+			Tuning t = new Tuning("disambi-tuning.txt");
 			// Tuning t = new Tuning("accuracy-pairs." + (i+1) + ".txt");
 			t.tune();
 		}
