640c074aba680cacea3f959846c90c76de409a7c
quyin <quyin@ecologylab.net>
2010-11-15 07:01:25 +0000

    scripts for distributed computation added.

--- scripts/lbsdmla.sh
+++ scripts/lbsdmla.sh
@@ -5,6 +5,11 @@ if (($# != 5)); then
 	echo "    <map-script> should accept 2 arguments: <input-file> <output-file>, to carry out computation;"
 	echo "    <reduce-script> should accept a list of file names to combine them into one single result file."
 else
+  for currentf in $(ls $3-split-*); do
+    echo "removing $currentf..."
+    rm -rf $currentf
+  done
+  
 	total_line_count=$(wc -l < $1)
 	split_line_count=$(($total_line_count / $2 + 1))
 	split -d -l $split_line_count $1 $3-split-
